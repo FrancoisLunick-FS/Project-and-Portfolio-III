adbd080539df7e7f09b8cfe5562fdd0a953a6cae
Lunick Francois <lunickfrancois@Lunicks-MacBook-Pro.local>
2021-07-17 00:58:56 -0400

    Created pod file

--- Francois_Lunick_Alpha/LetsChatAlpha/LetsChatAlpha/Controller/Authentication/RegistrationViewController.swift
+++ Francois_Lunick_Alpha/LetsChatAlpha/LetsChatAlpha/Controller/Authentication/RegistrationViewController.swift
@@ -6,13 +6,34 @@
 //
 
 import UIKit
+import Firebase
 
 class RegistrationViewController: UIViewController {
 
+    // MARK: - Properties
+    @IBOutlet weak var signUpButton: UIButton!
+    @IBOutlet weak var googleSignUpButton: UIButton!
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    
+    // MARK: - View Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        signUpButton.layer.cornerRadius = 20
+        googleSignUpButton.layer.cornerRadius = 20
+    }
+    
+    // MARK: - Actions
+    @IBAction func handleNewUser(_ sender: UIButton) {
+        
+        guard let name = nameTextField.text,
+              let email = emailTextField.text,
+              let password = passwordTextField.text else { return }
+        
+        
     }
     
 

0d9d11849b0f4fe3e2d2b1aa65573cbe40e0c0d6
Lunick Francois <lunickfrancois@Lunicks-MacBook-Pro.local>
2021-07-16 23:05:14 -0400

    Updated Storyboard

---
+++ Francois_Lunick_Alpha/LetsChatAlpha/LetsChatAlpha/Controller/Authentication/LoginViewController.swift
@@ -0,0 +1,34 @@
+//
+//  LoginViewController.swift
+//  LetsChatAlpha
+//
+//  Created by Lunick Francois on 7/16/21.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var signInButton: UIButton!
+    @IBOutlet weak var googleSignInButton: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        signInButton.layer.cornerRadius = 20
+        googleSignInButton.layer.cornerRadius = 20
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Francois_Lunick_Alpha/LetsChatAlpha/LetsChatAlpha/Controller/ChatViewController.swift
+++ Francois_Lunick_Alpha/LetsChatAlpha/LetsChatAlpha/Controller/ChatViewController.swift
@@ -9,25 +9,54 @@ import UIKit
 
 class ChatViewController: UIViewController {
 
+    // MARK: - Properties
+    @IBOutlet weak var tableView: UITableView!
+    
+    // MARK: - View Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        configureUI()
+        
     }
     
+    // MARK: - Helpers
+    func configureUI() {
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationItem.title = "Chats"
+//        navigationController?.navigationBar.tintColor = .white
+//        navigationController?.navigationBar.isTranslucent = true
+//
+//        navigationController?.navigationBar.overrideUserInterfaceStyle = .dark
+        
+        configureTableView()
+        
+    }
     
-
+    func configureTableView() {
+        tableView.rowHeight = 80
+        
+    }
+    
+    // MARK: - Actions
+    @IBAction func presentNewMessage(_ sender: UIBarButtonItem) {
+        
+    }
+    
 }
 
 // MARK: - UITableViewDataSource
 extension ChatViewController: UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        <#code#>
+        return 1
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        <#code#>
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell_id00", for: indexPath)
+        
+        return cell
     }
     
     
